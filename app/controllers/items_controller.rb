2d4c3c8c273c6e5b7008a94e95cf81b2636edc91
Yevgeniy Ivanov <anigeka@gmail.com>
2018-06-24 18:08:49 -0500

    checkpoint complete v1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,16 +5,15 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item_name = Item.find_by(params[:name])
+    @item_name = params[:item][:name]
     p "------------"
     p @item_name
     p "------------"
     if current_user.items.create!(name: @item_name, user: current_user)
       flash[:notice] = "The item was created"
-      redirect_to root_path
     else
       flash[:notice] = "The item could not be created"
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 end
