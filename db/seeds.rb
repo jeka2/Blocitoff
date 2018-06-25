0519ad1de5208bae0aef46c3ffaf7a454e8d6e62
Yevgeniy Ivanov <anigeka@gmail.com>
2018-06-24 20:02:52 -0500

    checkpoint complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
-User.create!(
+test_user = User.create!(
   email: "a@a.com",
   password: "password",
   confirmed_at: Time.now
 )
+
+50.times do
+  Item.create!(
+    name: Faker::Simpsons.character,
+    user: test_user
+)
+end
