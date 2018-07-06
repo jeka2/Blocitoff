96511fc7f3c88eccca83307283080fe25ada8f38
Yevgeniy Ivanov <anigeka@gmail.com>
2018-07-06 15:27:07 -0500

    slack

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,19 +3,16 @@ class ItemsController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @item = Item.new
-    respond_to do |format|
-      format.json {render layout: false}
-    end
   end
 
   def create
     @item_name = params[:item][:name]
     if current_user.items.create!(name: @item_name, user: current_user)
       flash[:notice] = "The item was created"
+      @last_item = Item.last
     else
       flash[:notice] = "The item could not be created"
     end
-    redirect_to root_path
   end
 
   def destroy
