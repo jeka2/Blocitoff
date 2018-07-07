9abf777463df7c7427d4bb3f8f945fbd463736c8
Yevgeniy Ivanov <anigeka@gmail.com>
2018-07-06 21:08:30 -0500

    checkpoint complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item_name = params[:item][:name]
-    if current_user.items.create!(name: @item_name, user: current_user)
+    @item = Item.new(name: params[:item][:name], user: current_user)
+    if @item.save
       flash[:notice] = "The item was created"
     else
       flash[:notice] = "The item could not be created"
@@ -16,5 +16,10 @@ class ItemsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:id])
+    if @item.destroy
+      flash[:notice] = "The item was deleted"
+    else
+      flash[:notice] = "The item could not be deleted"
+    end
   end
 end
