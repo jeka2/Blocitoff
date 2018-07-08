94235168858a6af8a6acae6380584cae377a9e97
Yevgeniy Ivanov <anigeka@gmail.com>
2018-07-08 15:50:26 -0500

    checkpoint complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def converter(expire_time)
-    distance_of_time_in_words(Time.now, expire_time)
-  end
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+  def time_left(expire_time)
+    if Time.now.to_f <= expire_time
+      "#{distance_of_time_in_words(Time.now, expire_time)} left"
+    else
+      "The task has expired"
+    end
+  end
 end
