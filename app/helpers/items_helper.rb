e6b566d994dcdbb167fdd873ddd31eaa78f8e154
Yevgeniy Ivanov <anigeka@gmail.com>
2018-07-08 16:18:36 -0500

    another slight revision

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,6 +1,6 @@
 module ItemsHelper
   def time_left(expire_time)
-    if Time.now.to_f <= expire_time
+    if Time.now <= expire_time
       "#{distance_of_time_in_words(Time.now, expire_time)} left to complete"
     else
       "The task has expired"
